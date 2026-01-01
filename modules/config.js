df2ece1a5f4fcce1973265a5f03f44be3786592b
stripe22 <suarezcartas@gmail.com>
2026-01-01 13:55:35 -0500

    2

--- modules/config.js
+++ modules/config.js
@@ -1,8 +1,8 @@
 // modules/config.js
 export const CONFIG = {
   // Pega aquí tu URL y ANON
-  SUPABASE_URL: "PON_AQUI_TU_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "PON_AQUI_TU_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://gngvxftfpppbauvbyhpr.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduZ3Z4ZnRmcHBwYmF1dmJ5aHByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyNDYyMjcsImV4cCI6MjA4MjgyMjIyN30.AUGg3hur411LJWOYWp93-DG_nCUGDfuTNrWIdrG2RZI",
 
   // Opción B: fallback hardcode
   PRINTER_KEY_FALLBACK: "a9670d76f517cfbd329c01397234a1717c87cc22145ba3ea39da035f2219add4",
